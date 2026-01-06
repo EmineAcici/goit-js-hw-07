2368ac50ff065b275b3cd9df869bbd51f18387b2
emine-busra <eminebusraacici@hotmail.com>
2026-01-06 18:07:02 +0300

    Made last task

---
+++ js/task-6.js
@@ -0,0 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const boxesContainer = document.querySelector("#boxes");
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let index = 0; index < amount; index++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.append(box);
+    size += 10;
+  }
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+  input.value = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
